22e3e77dd82271624aa361c9a5efd98c03155bf6
Amirmasoud <amirmasood33@gmail.com>
2025-08-26 19:22:30 -0600

    sorting

--- resources/js/components/table/Header.vue
+++ resources/js/components/table/Header.vue
@@ -1,51 +1,25 @@
 <template>
-    <UiDropdownMenu :content="{ align: 'start' }" aria-label="Actions dropdown" :items="items">
-        <UiButton
-            color="neutral"
-            variant="ghost"
-            :icon="isSorted ? (isSorted === 'asc' ? 'i-lucide-arrow-up-narrow-wide' : 'i-lucide-arrow-down-wide-narrow') : 'i-lucide-arrow-up-down'"
-            class="data-[state=open]:bg-elevated -mx-2.5"
-            aria-label="Sort by ${isSorted === 'asc' ? 'descending' : 'ascending'}"
-        >
-            {{ label }}
-        </UiButton>
-    </UiDropdownMenu>
+    <UiButton
+        color="neutral"
+        variant="ghost"
+        :icon="isSorted
+          ? isSorted === 'asc'
+            ? 'i-lucide-arrow-up-narrow-wide'
+            : 'i-lucide-arrow-down-wide-narrow'
+          : 'i-lucide-arrow-up-down'"
+        class="data-[state=open]:bg-elevated -mx-2.5"
+        aria-label="Sort by ${isSorted === 'asc' ? 'descending' : 'ascending'}"
+        @click="() => props.column.toggleSorting(props.column.getIsSorted() === 'asc')"
+    >
+        {{ label }}
+    </UiButton>
 </template>
 <script setup lang="ts">
 import type { Column } from '@tanstack/vue-table';
-import { ref } from 'vue';
 
 const props = defineProps<{
-    column: Column;
+    column: Column<any>;
     label: string;
 }>();
 const isSorted = props.column.getIsSorted();
-const items = ref([
-    {
-        label: 'Asc',
-        type: 'checkbox',
-        icon: 'i-lucide-arrow-up-narrow-wide',
-        checked: isSorted === 'asc',
-        onSelect: () => {
-            if (isSorted === 'asc') {
-                props.column.clearSorting();
-            } else {
-                props.column.toggleSorting(false);
-            }
-        },
-    },
-    {
-        label: 'Desc',
-        icon: 'i-lucide-arrow-down-wide-narrow',
-        type: 'checkbox',
-        checked: isSorted === 'desc',
-        onSelect: () => {
-            if (isSorted === 'desc') {
-                props.column.clearSorting();
-            } else {
-                props.column.toggleSorting(true);
-            }
-        },
-    },
-]);
 </script>
