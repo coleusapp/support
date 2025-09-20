1ae257ab33d3ec371b39a1047eee3551ff49c576
Amirmasoud <amirmasood33@gmail.com>
2025-09-20 16:13:37 -0600

    wip

--- base.config.js
+++ base.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
     build: { outDir: 'resources/dist' },
     plugins: [
         tailwindcss(),
+        laravel({
+            input: ['resources/js/app.ts'],
+            refresh: true,
+            hotFile: path.resolve(__dirname, '../../public/hot'),
+        }),
         vue({
             template: {
                 transformAssetUrls: {
---
+++ formkit.config.ts
@@ -0,0 +1,8 @@
+import { createProPlugin, repeater, toggle, togglebuttons } from '@formkit/pro';
+
+export const proPlugin = createProPlugin('fk-36c75313d', {
+    repeater,
+    toggle,
+    togglebuttons,
+});
+
