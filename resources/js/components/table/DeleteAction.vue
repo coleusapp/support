b51a84252d637205316695d45d67129e6d3e8003
Amirmasoud <amirmasood33@gmail.com>
2026-06-17 17:41:10 -0600

    Category delete button is disabled if there are exercised assigned to it

--- resources/js/components/table/DeleteAction.vue
+++ resources/js/components/table/DeleteAction.vue
@@ -3,12 +3,14 @@ import { Link } from '@inertiajs/vue3';
 
 defineProps<{
     href: string;
+    disabled?: boolean;
 }>();
 </script>
 <template>
-    <UiTooltip text="Delete">
-        <Link :href="href" method="delete" @success="() => $toast.add({ title: 'Deleted successfully!' })">
-            <UiButton icon="i-lucide-trash" variant="ghost" color="error"></UiButton>
+    <UiTooltip :text="disabled ? 'Cannot delete, currently in use' : 'Delete'">
+        <Link v-if="!disabled" :href="href" method="delete" @success="() => $toast.add({ title: 'Deleted successfully!' })">
+            <UiButton icon="i-lucide-trash" variant="ghost" color="error" />
         </Link>
+        <UiButton v-else icon="i-lucide-trash" variant="ghost" color="error" disabled />
     </UiTooltip>
 </template>
